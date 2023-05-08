e537157d14ce64f58a41b671d55263c459729493
danielyedaniel <yedaniel99@gmail.com>
2023-05-07 21:22:38 -0400

    init

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.student;
+
+import java.time.*;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args->{
+            Student mariam = new Student(
+				1L,
+				"Mariam",
+				"mariam.jamal@gmail.com",
+				LocalDate.of(2000, Month.JANUARY, 5)
+			);
+
+            Student alex = new Student(
+				2L,
+				"Alex",
+				"alex@gmail.com",
+				LocalDate.of(2004, Month.APRIL, 5)
+			);
+
+            repository.saveAll(
+                List.of(mariam,alex)
+            );
+        };
+    }
+}
